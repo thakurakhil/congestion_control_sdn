d09027258e9e85c511577349825cbe44bd077939
Akhil Singh <singhakhil33@gmail.com>
2021-04-19 13:13:48 +0530

    plots

--- plot_graphs.sh
+++ plot_graphs.sh
@@ -7,8 +7,8 @@ oldpwd=$PWD
 
 cd $results
 
-for (( i = 0; i <= $n_iperf_flows; i++ )) 
-do
+# for (( i = 0; i <= $n_iperf_flows; i++ )) 
+# do
 
 # echo "retriving bbr rtt for flow no:$(($i+1))..."
 # tshark -2 -r ./flow_bbr_$i.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./bbr_rtt_$i.txt
@@ -19,16 +19,16 @@ do
 # echo "retriving pcc rtt for flow no:$(($i+1))..."
 # tshark -2 -r ./flow_pcc_$i.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_$i.txt
 
-echo "retriving pcc rtt for flow_pcc_1..."
-tshark -2 -r ./flow_pcc_1.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_1.txt
-echo "retriving pcc rtt for flow_pcc_21..."
-tshark -2 -r ./flow_pcc_21.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_21.txt
-echo "retriving pcc rtt for flow_pcc_22..."
-tshark -2 -r ./flow_pcc_22.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_22.txt
-echo "retriving pcc rtt for flow_pcc_23..."
-tshark -2 -r ./flow_pcc_23.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_23.txt
-echo "retriving pcc rtt for flow_pcc_5..."
-tshark -2 -r ./flow_pcc_5.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_5.txt
+# echo "retriving pcc rtt for flow_pcc_1..."
+# tshark -2 -r ./flow_pcc_1.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_1.txt
+# echo "retriving pcc rtt for flow_pcc_21..."
+# tshark -2 -r ./flow_pcc_21.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_21.txt
+# echo "retriving pcc rtt for flow_pcc_22..."
+# tshark -2 -r ./flow_pcc_22.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_22.txt
+# echo "retriving pcc rtt for flow_pcc_23..."
+# tshark -2 -r ./flow_pcc_23.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_23.txt
+# echo "retriving pcc rtt for flow_pcc_5..."
+# tshark -2 -r ./flow_pcc_5.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e frame.time_relative -e tcp.analysis.ack_rtt -Tfields -E separator=, > ./pcc_rtt_5.txt
 
 # echo "retriving bbr throughput for flow no:$(($i+1))..."
 # captcp throughput -u Mbit --stdio flow_bbr_$i.dmp > captcp_bbr_$i.txt
@@ -43,34 +43,34 @@ tshark -2 -r ./flow_pcc_5.dmp -R 'tcp.stream eq 1 && tcp.analysis.ack_rtt'  -e f
 # awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_$i.txt > captcp-csv_pcc_$i.txt
 
 
-echo "retriving pcc throughput for flow_pcc_1..."
-captcp throughput -u Mbit --stdio flow_pcc_1.dmp > captcp_pcc_1.txt
-awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_1.txt > captcp-csv_pcc_1.txt
+# echo "retriving pcc throughput for flow_pcc_1..."
+# captcp throughput -u Mbit --stdio flow_pcc_1.dmp > captcp_pcc_1.txt
+# awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_1.txt > captcp-csv_pcc_1.txt
 
-echo "retriving pcc throughput for flow_pcc_21..."
-captcp throughput -u Mbit --stdio flow_pcc_21.dmp > captcp_pcc_21.txt
-awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_21.txt > captcp-csv_pcc_21.txt
+# echo "retriving pcc throughput for flow_pcc_21..."
+# captcp throughput -u Mbit --stdio flow_pcc_21.dmp > captcp_pcc_21.txt
+# awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_21.txt > captcp-csv_pcc_21.txt
 
-echo "retriving pcc throughput for flow_pcc_22..."
-captcp throughput -u Mbit --stdio flow_pcc_22.dmp > captcp_pcc_22.txt
-awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_22.txt > captcp-csv_pcc_22.txt
+# echo "retriving pcc throughput for flow_pcc_22..."
+# captcp throughput -u Mbit --stdio flow_pcc_22.dmp > captcp_pcc_22.txt
+# awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_22.txt > captcp-csv_pcc_22.txt
 
-echo "retriving pcc throughput for flow_pcc_23..."
-captcp throughput -u Mbit --stdio flow_pcc_23.dmp > captcp_pcc_23.txt
-awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_23.txt > captcp-csv_pcc_23.txt
+# echo "retriving pcc throughput for flow_pcc_23..."
+# captcp throughput -u Mbit --stdio flow_pcc_23.dmp > captcp_pcc_23.txt
+# awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_23.txt > captcp-csv_pcc_23.txt
 
-echo "retriving pcc throughput for flow_pcc_5..."
-captcp throughput -u Mbit --stdio flow_pcc_5.dmp > captcp_pcc_5.txt
-awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_5.txt > captcp-csv_pcc_5.txt
-
-
-# echo "plotting rtt graphs.."
-# python $oldpwd/plot_ping.py -f ./bbr_rtt_$i.txt ./cubic_rtt_$i.txt ./pcc_rtt_$i.txt --xlimit 8 -o ./rtt_$i.png
-
-# python $oldpwd/plot_throughput.py --xlimit 50 -f ./captcp-csv_bbr_$i.txt ./captcp-csv_cubic_$i.txt ./captcp-csv_pcc_$i.txt -o ./throughput_$1.png
+# echo "retriving pcc throughput for flow_pcc_5..."
+# captcp throughput -u Mbit --stdio flow_pcc_5.dmp > captcp_pcc_5.txt
+# awk "{print (\$1+$i*2-1)(\",\")(\$2) }" < captcp_pcc_5.txt > captcp-csv_pcc_5.txt
 
 
-done
+echo "plotting rtt graphs.."
+python $oldpwd/plot_ping.py -f ./pcc_rtt_1.txt ./pcc_rtt_21.txt ./pcc_rtt_22.txt ./pcc_rtt_23.txt ./pcc_rtt_5.txt --xlimit 8 -o ./rtt_1.png
+
+python $oldpwd/plot_throughput.py --xlimit 50 -f ./captcp-csv_pcc_1.txt ./captcp-csv_pcc_21.txt ./captcp-csv_pcc_22.txt ./captcp-csv_pcc_23.txt ./captcp-csv_pcc_5.txt -o ./throughput_1.png
+
+
+#done
 
 # echo "plotting throughput graphs.."
 # python $oldpwd/plot_throughput.py --xlimit 50 -f ./captcp-csv_bbr* -o ./throughput_bbr.png
